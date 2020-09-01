dd6338e1e2c66d41bd5913694f88e1be011d78f8
Jorge Massih <jorgmassih@gmail.com>
2020-09-01 16:10:41 -0400

    Config class added in mongo_driver.py MongoDriver class modified for automatic closing connection.

--- htmh_l2vpn/mongodb/mongo_driver.py
+++ htmh_l2vpn/mongodb/mongo_driver.py
@@ -2,9 +2,14 @@ from pymongo import MongoClient
 
 
 class MongoDriver():
-    def __init__(self, db_name):
+    def __init__(self, db_name: str):
         self.db_name = db_name
-        self.client = MongoClient(self.mongo_uri)
+
+    @property
+    def db(self):
+        with MongoClient(self.mongo_uri) as client:
+            db_ = client[self.db_name]
+        return db_
 
     @property
     def mongo_uri(self):
@@ -12,5 +17,22 @@ class MongoDriver():
         return mongo_uri
 
 
+class Config(MongoDriver):
+    def __init__(self, db_name: str, doc: str):
+        super().__init__(db_name)
+        self.collection = self.db['Config']
+        self.data_key = 'data'
+        self.doc = doc
+
+    def __getattr__(self, attr):
+        coll = self.collection.find_one({'type': self.doc})
+        data = coll[self.data_key]
+        if attr in data:
+            return data[attr]
+        else:
+            raise Exception('Invalid key')
+
+
 if __name__ == '__main__':
-    mongo = MongoDriver('NetworkStatus')
+    mongo = Config('NetworkStatus', 'connection')
+    print(mongo.url)
