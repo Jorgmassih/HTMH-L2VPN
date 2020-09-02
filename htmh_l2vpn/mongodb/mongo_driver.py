eb34e083fdac38e493557d8d243948ab10adc656
Jorge Massih <jorgmassih@gmail.com>
2020-09-02 19:18:57 -0400

    NetworkAnatomy class added in mongo_driver.py. GetConfig class incorporated to ONOS driver app.

--- htmh_l2vpn/mongodb/mongo_driver.py
+++ htmh_l2vpn/mongodb/mongo_driver.py
@@ -17,14 +17,27 @@ class MongoDriver():
         return mongo_uri
 
 
-class Config(MongoDriver):
-    def __init__(self, db_name: str, doc: str):
+class GetConfig(MongoDriver):
+    def __init__(self, db_name: str, collection: str = '', doc: str = ''):
         super().__init__(db_name)
-        self.collection = self.db['Config']
+        self._collection_ = collection
         self.data_key = 'data'
         self.doc = doc
 
+    @property
+    def collection(self):
+        if self._collection_ not in self.db.list_collection_names():
+            raise Exception('Invalid collection')
+        return self.db[self._collection_]
+
+    @collection.setter
+    def collection(self, attr):
+        self._collection_ = attr
+
     def __getattr__(self, attr):
+        if not self.doc:
+            raise Exception('You must set doc attribute first')
+
         coll = self.collection.find_one({'type': self.doc})
         data = coll[self.data_key]
         if attr in data:
@@ -33,6 +46,42 @@ class Config(MongoDriver):
             raise Exception('Invalid key')
 
 
+class NetworkAnatomy(MongoDriver):
+    def __init__(self, db_name: str):
+        super().__init__(db_name)
+
+    @property
+    def hosts(self):
+        return {}
+
+    @property
+    def access_devices(self):
+        return []
+
+    @property
+    def core_devices(self):
+        return []
+
+    @property
+    def links(self):
+        return []
+
+    @links.setter
+    def links(self, new_links: dict):
+        pass
+
+    def insert_host(self, host: dict):
+        pass
+
+    def insert_link(self, link: dict):
+        pass
+
+    def compare_links(self, link: dict):
+        pass
+
+
 if __name__ == '__main__':
-    mongo = Config('NetworkStatus', 'connection')
-    print(mongo.url)
+    mongo = GetConfig('NetworkStatus')
+    mongo.collection = 'Endpoints'
+    mongo.doc = 'hosts_info'
+    print(mongo.get_hosts)
--- htmh_l2vpn/onos/onos.py
+++ htmh_l2vpn/onos/onos.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from os import path
+from htmh_l2vpn.mongodb.mongo_driver import GetConfig
 
 import copy
 
@@ -8,13 +9,10 @@ import copy
 class ONOSDriver:
 
     def __init__(self):
-
-        config_file = self.__open_config_file(file='config.json')
-        api_config = config_file['api']
-
-        self.headers = api_config['headers']
-        self.auth = tuple(api_config['auth'])
-        self.url = api_config['url']
+        self.config = GetConfig(db_name='NetworkStatus', collection='Config', doc='connection')
+        self.headers = self.config.headers
+        self.auth = tuple(self.config.auth)
+        self.url = self.config.url
 
     @staticmethod
     def __open_config_file(file: str):
@@ -27,18 +25,20 @@ class ONOSDriver:
         return config_file
 
     def get_hosts(self):
-        config_file = self.__open_config_file(file='access_sw.json')
-        end_points = config_file['endpoints']
-        hosts = requests.get(end_points['hosts_info']['get_hosts'].format(self.url),
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'hosts_info'
+
+        hosts = requests.get(self.config.get_hosts.format(self.url),
                              headers=self.headers,
                              auth=self.auth).json()['hosts']
 
         return hosts
 
     def get_sw_public_mac(self, id: str):
-        config_file = self.__open_config_file(file='access_sw.json')
-        end_points = config_file['endpoints']
-        device_ports = requests.get(end_points["devices_info"]['get_device_ports'].format(self.url, id),
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'devices_info'
+
+        device_ports = requests.get(self.config.get_device_ports.format(self.url, id),
                                     headers=self.headers,
                                     auth=self.auth).json()['ports']
 
@@ -52,20 +52,24 @@ class ONOSDriver:
         return mac
 
     def install_intents(self, macs_pair: tuple):
-        config_file = self.__open_config_file(file='access_sw.json')
-        end_points = config_file['endpoints']
-        intent_flow = config_file['flows']['host2host_intent']
+        self.config.collection = 'Flows'
+        self.config.doc = 'host2host_intent'
+
+        intent_flow = self.config.intent_body
         intent_flow['one'] = macs_pair[0] + '/-1'
         intent_flow['two'] = macs_pair[1] + '/-1'
 
-        response = requests.post(end_points['intents']['post_intent'].format(self.url),
-                      headers=self.headers, auth=self.auth, json=intent_flow)
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'intents'
+
+        response = requests.post(self.config.post_intent.format(self.url), headers=self.headers, auth=self.auth,
+                                 json=intent_flow)
 
         return response
 
     def install_arp_flow(self, device_id: str, hosts: dict, ports: list):
-        config_file = self.__open_config_file(file='access_sw.json')
-        end_points = config_file['endpoints']
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'network_config'
 
         interfaces = []
 
@@ -85,14 +89,15 @@ class ONOSDriver:
             location = "{}/{}".format(device_id, port)
             flow.update({location: {"interfaces": interfaces}})
 
-        response = requests.post(end_points['network_config']['ports'].format(self.url),
+        response = requests.post(self.config.ports.format(self.url),
                                  headers=self.headers, auth=self.auth, json=flow)
 
         return response
 
     def install_outgoing_flows(self, device: str, hosts: dict, foreign_hosts: dict, service_token: str):
-        config_file = self.__open_config_file(file='access_sw.json')
-        end_points = config_file['endpoints']
+        self.config.collection = 'Flows'
+        self.config.doc = 'outgoing_traffic'
+        flow_body = self.config.flow
 
         flow_list = []
 
@@ -101,7 +106,7 @@ class ONOSDriver:
 
         for foreign_mac in foreign_macs:
             for host_mac in hosts_macs:
-                flow = copy.deepcopy(config_file['flows']['outgoing_traffic'])
+                flow = copy.deepcopy(flow_body)
 
                 flow['deviceId'] = device
                 flow['treatment']['instructions'][0]['mac'] = foreign_hosts[foreign_mac]['public_mac']
@@ -114,21 +119,25 @@ class ONOSDriver:
 
         flow = {"flows": flow_list}
 
-        response = requests.post(end_points['flows']['post_flow'].format(self.url, 'l2vpn.outgoing.map.' + service_token),
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'flows'
+
+        response = requests.post(self.config.post_flow.format(self.url, 'l2vpn.outgoing.map.' + service_token),
                                  headers=self.headers, auth=self.auth, json=flow)
 
         return response
 
     def install_incoming_flows(self, device: str, hosts: dict, service_token: str):
-        config_file = self.__open_config_file(file='access_sw.json')
-        end_points = config_file['endpoints']
+        self.config.collection = 'Flows'
+        self.config.doc = 'incoming_traffic'
+        flow_body = self.config.flow
 
         flow_list = []
 
         macs = list(hosts.keys())
 
         for mac in macs:
-            flow = copy.deepcopy(config_file['flows']['incoming_traffic'])
+            flow = copy.deepcopy(flow_body)
 
             flow['deviceId'] = device
 
@@ -143,27 +152,33 @@ class ONOSDriver:
 
         flows = {"flows": flow_list}
 
-        response = requests.post(end_points['flows']['post_flow'].format(self.url, 'l2vpn.incoming.map.' + service_token),
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'flows'
+        response = requests.post(self.config.post_flow.format(self.url, 'l2vpn.incoming.map.' + service_token),
                                  headers=self.headers, auth=self.auth, json=flows)
 
         return response
 
     def install_shortest_path(self, src: str, dst: str, dst_public_mac: str, service_token: str):
-        config_file = self.__open_config_file(file='backbone_sw.json')
-        end_points = config_file['endpoints']
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'paths'
 
         src = "%3A".join(src.split(':'))
         dst = "%3A".join(dst.split(':'))
 
-        shortest_path = requests.get(end_points['paths']['get_shortest_path'].format(self.url, src, dst),
+        shortest_path = requests.get(self.config.get_shortest_path.format(self.url, src, dst),
                                      headers=self.headers, auth=self.auth).json()
 
         shortest_path = shortest_path['paths'][0]['links']
 
         flow_list = []
 
+        self.config.collection = 'Flows'
+        self.config.doc = 'fwd_path'
+        flow_body = self.config.flow
+
         for link in shortest_path:
-            flow = copy.deepcopy(config_file['flows']['fwd_path'])
+            flow = copy.deepcopy(flow_body)
             output_port = link['src']['port']
             device_id = link['src']['device']
 
@@ -176,16 +191,18 @@ class ONOSDriver:
 
         flow = {"flows": flow_list}
 
-        response = requests.post(end_points['flows']['post_flow'].format(self.url, 'l2vpn.core.path.' + service_token),
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'flows'
+        response = requests.post(self.config.post_flow.format(self.url, 'l2vpn.core.path.' + service_token),
                                  headers=self.headers, auth=self.auth, json=flow)
 
         return response
 
     def delete_flow_app(self, app_id: str):
-        config_file = self.__open_config_file(file='config.json')
-        end_points = config_file['api']['endpoints']
-        response = requests.delete(end_points["delete_flows"]["delete_application"][1].format(self.url, app_id),
-                                    headers=self.headers,
-                                    auth=self.auth)
+        self.config.collection = 'Endpoints'
+        self.config.doc = 'flows'
+
+        response = requests.delete(self.config.delete_flows.format(self.url, app_id), headers=self.headers,
+                                   auth=self.auth)
 
         return response
